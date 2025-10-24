1480792d189887eaf46fd6a5d68f4503e54b365e
Student <student@naa.local>
2025-10-24 10:34:15 +0400

    Task

---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,29 @@
+﻿using Task1;
+
+UserService userService = new UserService();
+
+User newUser1 = new User(4, "Osman", "osman@email.com");
+userService.Create(newUser1, out bool created);
+
+if (created)
+{
+    Console.WriteLine($"User yaradıldı, Id: {newUser1.id}");
+}
+
+User newUser2 = new User(6, "Osman", "osman@email.com");
+userService.Create(newUser2, out bool created2);
+
+if (created2)
+{
+    Console.WriteLine($"User yaradıldı, Id: {newUser2.id}");
+
+}
+
+User[] Users = userService.GetAll();
+
+foreach (User user in Users)
+{
+    Console.WriteLine(user);
+}
+
+
---
+++ ConsoleApp16/UserService.cs
@@ -0,0 +1,93 @@
+﻿namespace Task1
+{
+    internal class UserService
+    {
+        private User[] _users;
+
+        public UserService()
+        {
+            _users = new User[]
+            {
+                new User(1, "Ali","ali@email.com"),
+                new User(2, "Mehemmed","mehemmmed@email.com"),
+                new User(3, "Zohrab","zohrab@email.com"),
+                new User(4, "Ehmed","ehmed@email.com"),
+                new User(5, "Kenan","kenan@email.com"),
+            };
+        }
+        public void Create(User user, out bool IsLogged)
+        {
+            
+            for (int i = 0; i < _users.Length; i++)
+            {   
+                if(user.id == _users[i].id)
+                {
+                    Console.WriteLine("User artiq movcuddur");
+                    IsLogged = false;
+                    return;
+                }
+                
+
+            }
+            Array.Resize(ref _users, _users.Length+1);
+            _users[_users.Length-1] = user;
+            IsLogged = true;
+
+        }
+
+        public User GetById(int id)
+        {
+            foreach (var user in _users)
+            {
+                if(user.id == id) return user;  
+            }
+            return null;
+        }
+
+        public User[] GetAll()
+        {
+            return _users;
+        }
+
+        public void Delete(int id)
+        {
+            int idx = -1;
+            for (int i = 0;i<_users.Length;i++)
+            {
+                if (_users[i].id == id)
+                {
+                    break;
+                }
+            }
+            if(idx == -1)
+            {
+                Console.WriteLine("User tapilmadi");
+            }
+            for (int i = idx; i < _users.Length; i++)
+            {
+                _users[i] = _users[i + 1];
+            }
+            Array.Resize(ref _users, _users.Length - 1);
+            
+        }
+    }
+    public class User
+    {
+        public int id { get; }
+        public string name { get; set; }
+
+        public string email { get; set; }
+
+        public User(int id, string name, string email)
+        {
+            this.id = id;
+            this.name = name;
+            this.email = email;
+        }
+        public override string ToString()
+        {
+            return $"Id: {id}, Name: {name}, Email: {email}";
+        }
+
+    }
+}
